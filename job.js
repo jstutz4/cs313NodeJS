a3eb7cdaa84f13ec7878fdb158bf086c47ddcaa2
Jon STuTZ <stu15012@byui.edu>
2019-07-06 12:18:12 -0700

    db

--- job.js
+++ job.js
@@ -138,7 +138,7 @@ function updateStocks(req, res){
         console.log("Successfully Written to File.");
     }); 
     // add stock into db
-    const stock = req.params.stock;
+    const stock = res.params.stock;
     //insertIntoTable("stocks",stock,function(req, res){
      // loop through the db and collect all the stock symbols
         getAllFromTable("stocks", "symbol", function(err, stocks){
