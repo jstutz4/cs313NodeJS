c5c21f3a3704e24ecfef9d64c883154bb00b7b09
Jon STuTZ <stu15012@byui.edu>
2019-07-23 14:42:52 -0700

    will it work

--- stylesheets/js/home.js
+++ stylesheets/js/home.js
@@ -32,57 +32,7 @@ function getSearch() {
     apiSearch(search, searchStock);
 }
 
-function getAllStock(stock) {
 
-    var stringObj;
-    var obj;
-    var table = document.getElementById('tTable').innerHTML;
-    var button;
-    var symbol_list = "";
-    var length;
-    var i = 0;
-   
-    if(document.getElementById('tTable').children != null){
-        symbol_list = document.getElementById('tTable').children;
-    }
-    console.log(symbol_list)
-    console.log('look at i ');
-    console.log(symbol_list.length);
-    if(symbol_list.length == 0){
-        length = 0;
-        i = -1;
-    }else{
-        length = symbol_list.length;
-        i = -1;
-    }
-  
-    
-    for (let j = i; j < length; j++) {
-        console.log('i: ' + i + " j: " + j);
-        if (j == -1) {
-            console.log('we made is in the loop');
-            // console.log('before');
-            // console.log(stock);
-            apiSearch(stock, displayTable);
-            // console.log('after');
-            // obj = stock;
-            // button = '<input type="button" name="' + obj.symbol + '" value="Track Stock" onclick="investStock(this)">';
-            // table += '<tr class=' + obj.symbol + '"><td name="' + obj.symbol + '">' + obj.symbol + '</td><td name="' + obj.symbol + '">' + obj.latestPrice + '</td><td name="' + obj.symbol + '">' + ((obj.latestVolume / 1000000).toFixed(2)) + "</td><td>" + button + "</td></tr>";
-       // next();
-        }
-        else{
-             console.log('we made it in the 2 loop');
-            // console.log(symbol_list[j].className);
-             apiSearch(symbol_list[j].className, displayTable);
-            // obj = stock
-            // button = '<input type="button" name="' + obj.symbol + '" value="Track Stock" onclick="investStock(this)">';
-            // table += '<tr class=' + obj.symbol + '"><td name="' + obj.symbol + '">' + obj.symbol + '</td><td name="' + obj.symbol + '">' + obj.latestPrice + '</td><td name="' + obj.symbol + '">' + ((obj.latestVolume / 1000000).toFixed(2)) + "</td><td>" + button + "</td></tr>";
-        }
-        //runInNewContext();
-    }
-    console.log("**table ** " + table);
-    document.getElementById("tTable").innerHTML = table;
-}
 
 function searchStock(obj) {
     var hTable = '<table><tr><th>symbol</th><th>price</th><th>volume (M)</th><th>High</th><th>Low</th><th>Change (%)</th><th>Action</th></tr>';
