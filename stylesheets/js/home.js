11cb753ca1a6c6d0602d1a91fde60ea90efcad0b
Jon STuTZ <stu15012@byui.edu>
2019-07-23 12:26:15 -0700

    keep trying

--- stylesheets/js/home.js
+++ stylesheets/js/home.js
@@ -192,7 +192,9 @@ function getInvest(obj){
             }
             if(!duplicate){
                 table += '<tr class="'+symbol +'" ><td>'+symbol+'</td><td id="ns'+symbol+'">'+numStocks+'</td><td id="a'+symbol + '">'+amount+'</td><td id="dollar'+symbol+'">$'+((numStocks* obj.latestPrice.toFixed(2)).toFixed(3)-amount).toFixed(2)+'</td><td id="pre'+symbol+'">0.00%</td><td>'+sell+'</td></tr>';
-                console.log((numStocks, obj.latestPrice.toFixed(2),amount));
+                console.log(numStocks);
+                console.log(obj.latestPrice);
+                console.log(amount);
                 document.getElementById('iTable').innerHTML = table;
             }
         }
