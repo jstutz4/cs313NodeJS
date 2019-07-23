d942834ef96e924ce131a85d0ef49d44dc5cc48c
Jon STuTZ <stu15012@byui.edu>
2019-07-23 13:00:54 -0700

    keep trying

--- stylesheets/js/home.js
+++ stylesheets/js/home.js
@@ -360,13 +360,13 @@ function getAllInvestCallback(price, row){
             console.log(table);
             //for(let i = 0; i < row.length; i++){
                 sell = '<input type="button" value="Sell Stock" class="'+row.symbol+'" onclick="sellInvest(this)">';
-                if(i == 0){
-                //table = '<tr class="'+row[i].symbol +'" ><td>'+row[i].symbol+'</td><td id="ns'+row[i].symbol+'">'+row[i].numstocks+'</td><td id="a'+row[i].symbol + '">'+row[i].amount+'</td><td id="dollar'+row[i].symbol+'">$'+((row[i].numStocks* obj.latestPrice)-row[i].amount).toFixed(2)+'</td><td id="pre'+row[i].symbol+'">'+((((Number(row[i].numstocks)*Number(obj.latestPrice))/Number(row[i].amount))-1)*100).toFixed(2) + '%';+'</td><td>'+sell+'</td></tr>';
-                table = '<tr class="'+row.symbol +'" ><td>'+row.symbol+'</td><td id="ns'+row.symbol+'">'+row.numstocks+'</td><td id="a'+row.symbol + '">'+row.amount+'</td><td id="dollar'+row.symbol+'">$'+(Math.round((row.numStocks* price)-row.amount)).toFixed(2)+'</td><td id="pre'+row.symbol+'">'+((((Number(row.numstocks)*Number(price))/Number(row.amount))-1)*100).toFixed(2) + '%';+'</td><td>'+sell+'</td></tr>';
-                }
-               else{
-                    table += '<tr class="'+row.symbol +'" ><td>'+row.symbol+'</td><td id="ns'+row.symbol+'">'+row.numstocks+'</td><td id="a'+row.symbol + '">'+row.amount+'</td><td id="dollar'+row.symbol+'">$'+((row.numStocks* price)-row.amount).toFixed(2)+'</td><td id="pre'+row.symbol+'">'+((((Number(row.numstocks)*Number(price))/Number(row.amount))-1)*100).toFixed(2) + '%';+'</td><td>'+sell+'</td></tr>';
-                }
+            //     if(i == 0){
+            //     //table = '<tr class="'+row[i].symbol +'" ><td>'+row[i].symbol+'</td><td id="ns'+row[i].symbol+'">'+row[i].numstocks+'</td><td id="a'+row[i].symbol + '">'+row[i].amount+'</td><td id="dollar'+row[i].symbol+'">$'+((row[i].numStocks* obj.latestPrice)-row[i].amount).toFixed(2)+'</td><td id="pre'+row[i].symbol+'">'+((((Number(row[i].numstocks)*Number(obj.latestPrice))/Number(row[i].amount))-1)*100).toFixed(2) + '%';+'</td><td>'+sell+'</td></tr>';
+            //     table = '<tr class="'+row.symbol +'" ><td>'+row.symbol+'</td><td id="ns'+row.symbol+'">'+row.numstocks+'</td><td id="a'+row.symbol + '">'+row.amount+'</td><td id="dollar'+row.symbol+'">$'+(Math.round((row.numStocks* price)-row.amount)).toFixed(2)+'</td><td id="pre'+row.symbol+'">'+((((Number(row.numstocks)*Number(price))/Number(row.amount))-1)*100).toFixed(2) + '%';+'</td><td>'+sell+'</td></tr>';
+            //     }
+            //    else{
+            //         table += '<tr class="'+row.symbol +'" ><td>'+row.symbol+'</td><td id="ns'+row.symbol+'">'+row.numstocks+'</td><td id="a'+row.symbol + '">'+row.amount+'</td><td id="dollar'+row.symbol+'">$'+((row.numStocks* price)-row.amount).toFixed(2)+'</td><td id="pre'+row.symbol+'">'+((((Number(row.numstocks)*Number(price))/Number(row.amount))-1)*100).toFixed(2) + '%';+'</td><td>'+sell+'</td></tr>';
+            //     }
                 table += '<tr class="'+row.symbol +'" ><td>'+row.symbol+'</td><td id="ns'+row.symbol+'">'+row.numstocks+'</td><td id="a'+row.symbol + '">'+row.amount+'</td><td id="dollar'+row.symbol+'">$'+((row.numStocks* price)-row.amount).toFixed(2)+'</td><td id="pre'+row.symbol+'">'+((((Number(row.numstocks)*Number(price))/Number(row.amount))-1)*100).toFixed(2) + '%';+'</td><td>'+sell+'</td></tr>';
             console.log('\nlisting info\n');
 
